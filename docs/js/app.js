970c4dce84734b7472138347bcf82e297b1e4ff9
Darshan Gada <gadadarshan@gmail.com>
2019-10-08 17:54:06 +0530

    Fix sw.js location

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,7 +2,7 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         // register the service worker script
-        .register('/sw.js')
+        .register('sw.js')
         // using promises tell us if successful or there was an error
         .then(reg => {console.info('Service Worker registration successful: ', reg)})
         .catch(err => {console.warn('Service Worker setup failed: ', err)});
