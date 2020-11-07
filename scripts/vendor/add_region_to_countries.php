be67e4669ecff62462e2e898b9a4a7a220855f79
Valerian Pereira <valerianpereira25@gmail.com>
2020-11-07 19:40:42 +0530

    Timezone changes

--- scripts/vendor/add_region_to_countries.php
+++ scripts/vendor/add_region_to_countries.php
@@ -3,7 +3,7 @@ require_once 'base.php';
 
 $start_time = time();
 
-$wikipediaJson = file_get_contents("data/wikipedia_tz.json");
+$wikipediaJson = file_get_contents("raw/wikipedia_tz.json");
 $wikipediaArray = json_decode($wikipediaJson, true);
 
 foreach($wikipediaArray as $wElemts) :
@@ -50,8 +50,8 @@ foreach($getAllTimeZonesArray['zones'] as $key => $zones) :
     $temp['zoneName'] = $zoneInfoJson->zoneName;
     $temp['gmtOffset'] = $zoneInfoJson->gmtOffset;
     $temp['gmtOffsetName'] = $zonePlace;
-    $temp['abbreviation'] = $zoneInfoJson->abbreviation;
-    $temp['tzName'] = $wkData[$search]['name'];
+    $temp['abbr'] = $zoneInfoJson->abbreviation;
+    $temp['tzName'] = (isset($wkData[$search]['name']) ? $wkData[$search]['name'] : "");
 
     echo '-------------------------------------'.PHP_EOL;
     echo 'RECORD NO: '. ($key + 1) .'/'.$totalRecords.PHP_EOL;
@@ -83,77 +83,18 @@ $fp = fopen('timezone_data.json', 'a') or die("Unable to open file!");
 fwrite($fp, json_encode((array)$countryTZ, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT|JSON_NUMERIC_CHECK).PHP_EOL);
 fclose($fp);
 
-echo 'Written to file: timezone_data.json in '.(time() - $start_time).' seconds'.PHP_EOL;
+// Update kar lo
+foreach ($countryTZ as $key => $value) : 
+    $tzData = mysqli_real_escape_string($conn, json_encode($value['timezones']));
+    $sql = "UPDATE countries 
+    SET timezones = '$tzData'
+    WHERE iso2 = '$key';";
 
-die();
-
-$regionsJson = file_get_contents("data/regions_tz.json");
-// print_r($regionsJson);
-$regionsArray = json_decode($regionsJson, true);
-// print_r($countriesArray);
-
-$wikipediaJson = file_get_contents("http://api.timezonedb.com/v2.1/list-time-zone?key=WTU4LLBZ20OF&format=json");
-$wikipediaArray = json_decode($wikipediaJson, true);
-echo '<pre>';
-print_r($wikipediaArray);
-die();
-
-echo '-------------------------------------'.PHP_EOL;
-echo 'JSON Data Starts'.PHP_EOL;
-echo '-------------------------------------'.PHP_EOL;
-foreach($regionsArray as $region) :
-    echo $region['name']." - ".$region['alpha3Code'].PHP_EOL;
+    if ($conn->query($sql) === TRUE) {
+        echo "Record updated successfully for ". $value['name'].PHP_EOL;
+    } else {
+        echo "Error: " . $sql ." ". $conn->error.PHP_EOL;
+    }
 endforeach;
-echo '-------------------------------------'.PHP_EOL;
-echo 'JSON Data Ends'.PHP_EOL;
-echo '-------------------------------------'.PHP_EOL;
 
-updateCountryTable($regionsArray, $conn);
-
-$conn->close();
-
-function updateCountryTable($regionsArray, $conn) {
-	echo '-------------------------------------'.PHP_EOL;
-	echo 'Records Check Starts'.PHP_EOL;
-	echo '-------------------------------------'.PHP_EOL;
-
-    foreach($regionsArray as $region) :
-
-        echo 'Checking For : '.$region['name']." ".$region['alpha3Code'].PHP_EOL;
-
-        $sql = "SELECT id FROM countries WHERE iso3='".$region['alpha3Code']."'";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) { // If Found Update It
-            echo 'Found... Updating...'.PHP_EOL;
-            while($row = $result->fetch_assoc()) {
-                $sql = "UPDATE countries SET region='".$region['region']."',subregion='".$region['subregion']."' WHERE id=".$row['id'];
-                if ($conn->query($sql) === TRUE) {
-                    echo "Record updated successfully".PHP_EOL;
-                } else {
-                    echo "Error updating record: ".$sql." ".$conn->error.PHP_EOL;
-                }
-            }
-        } else { // Else Insert it
-            echo 'Not Found... Creating...'.PHP_EOL;
-
-            extract($country);
-            $name = mysqli_real_escape_string($conn,$name);
-            $currency = $currencyCodes[0];
-
-            // $sql = "INSERT INTO countries (name, iso2, currency, created_at, wikiDataId) VALUES ('$name', '$code', '$currency', NOW(), '$wikiDataId')";
-
-            // if ($conn->query($sql) === TRUE) {
-            //     echo "New record created successfully".PHP_EOL;
-            // } else {
-            //     echo "Error: " . $sql ." ". $conn->error.PHP_EOL;
-            // }
-        }
-
-        echo PHP_EOL;
-    endforeach;
-
-    echo '-------------------------------------'.PHP_EOL;
-    echo 'Records Check Ends'.PHP_EOL;
-    echo '-------------------------------------'.PHP_EOL;
-}
+echo 'Written to file: timezone_data.json in '.(time() - $start_time).' seconds'.PHP_EOL;
