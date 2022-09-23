f125916ad2ebaf32f3cd9684040ec18a0e84b907
ionecum <77550203+ionecum@users.noreply.github.com>
2022-09-23 01:34:39 -0400

    Final Fixes on Italian Fix

--- scripts/vendor/base.php
+++ scripts/vendor/base.php
@@ -6,7 +6,7 @@ ini_set('error_reporting', E_ALL & ~E_NOTICE);
 date_default_timezone_set('Asia/Kolkata');
 
 // comment the following if you want to just export
-//require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 
 header('Content-type: text/plain');
 
