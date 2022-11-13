e320bface74cce57003200701e5259e9f0b97744
Darshan Gada <gadadarshan@gmail.com>
2022-11-13 20:44:28 +0530

    chore: update scripts

--- scripts/vendor/fix_duplicates.php
+++ scripts/vendor/fix_duplicates.php
@@ -1,32 +1,36 @@
 <?php
 require_once 'base.php';
 
-$countryId = 101; // India
+$countryCode = 'SA'; // India
+$stateCode = '04';
 
 // Step 1: Find Duplicates
-$sql = "SELECT name, country_id, state_id FROM cities WHERE country_id=$countryId ORDER BY country_id, state_id, name";
+$sql = "SELECT id, name, country_id, state_id FROM cities WHERE country_code='$countryCode' AND state_code='$stateCode' ORDER BY country_id, state_id, name";
 $cities = $conn->query($sql);
+
+// Grab the JSON File
+$file_name = "data/cities/" . $countryCode . '_' . $stateCode . '.json';
+
+$citiesJson = file_get_contents($file_name);
+$citiesArray = json_decode($citiesJson, true);
+$citynames = array_column($citiesArray, 'name');
+print_r($citynames);
+
+$ids = [];
 
 if ($cities->num_rows > 0) {
     while ($city = $cities->fetch_assoc()) {
-        $countryId = (int)$city['country_id'];
-        $stateId = (int)$city['state_id'];
-        // $name = $city['name'];
-        $name = implode('%', str_split($city['name']));
-        $sqld = "SELECT id, name FROM cities WHERE country_id=$countryId AND state_id=$stateId AND name LIKE '".$name."' ORDER BY id";
-        $cityResult = $conn->query($sqld);
-        if ($cityResult->num_rows > 1) {
-            echo 'Checking for: '.$city['name'].PHP_EOL;
-            echo '-----------'.PHP_EOL;
-            echo 'SQL: '.$sqld.PHP_EOL;
-            echo 'Found Duplicate....'.PHP_EOL;
-            while ($cr = $cityResult->fetch_assoc()) {
-                // Write what to do with duplicate
-                echo 'Duplicate Name & ID: '.$cr["name"].' ['.$cr["id"].']'.PHP_EOL;
-            }
-            echo ''.PHP_EOL.PHP_EOL;
+        $name = $city['name'];
+        $key = array_search($name, $citynames);
+        if ($key || $key === 0) {
+            // echo $name . ' ' . $key . PHP_EOL;
+        } else {
+            echo $city['id'] . ' ' . $name . ' not found' . PHP_EOL;
+            array_push($ids, $city['id']);
         }
     }
+
+    echo implode(',', $ids);
 }
 
 $conn->close();
