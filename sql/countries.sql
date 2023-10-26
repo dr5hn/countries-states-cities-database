ff46fe46a1203081a4043fc1e9c49b23e92f879f
dr5hn <dr5hn@users.noreply.github.com>
2023-10-26 07:57:21 +0000

    Add exported JSON, CSV, XML & YAML files
    Signed-off-by: Darshan Gada <gadadarshan@gmail.com>

--- sql/countries.sql
+++ sql/countries.sql
@@ -75,4 +75,4 @@ INSERT INTO `countries` VALUES (1,'Afghanistan','AFG','004','AF','93','Kabul','A
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-18 13:36:14
+-- Dump completed on 2023-10-26  7:57:12
