400e60181159e36881b497278a9b5a0814129022
Darshan Gada <gadadarshan@gmail.com>
2025-05-28 13:34:13 +0530

    Improve Workflow (#1033)
    Co-authored-by: dr5hn <6929121+dr5hn@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- psql/subregions.sql
+++ psql/subregions.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 16.8 (Ubuntu 16.8-1.pgdg24.04+1)
--- Dumped by pg_dump version 16.8 (Ubuntu 16.8-1.pgdg24.04+1)
+-- Dumped from database version 16.9 (Ubuntu 16.9-1.pgdg24.04+1)
+-- Dumped by pg_dump version 16.9 (Ubuntu 16.9-1.pgdg24.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
