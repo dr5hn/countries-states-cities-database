5d967addd44da4712d76ec555f6b8568f8fbe923
dr5hn <6929121+dr5hn@users.noreply.github.com>
2025-08-06 10:02:16 +0000

    📦 Export database formats - 06th Aug 2025
    ✅ All export formats completed successfully:
    - JSON/XML/YAML/CSV: Structured data exports
    - MySQL/PostgreSQL: SQL dump exports
    - SQLite: Database file exports
    - SQL Server/MongoDB: Alternative format exports
    
    📊 Total records: 250 countries, 5134 states, 151901 cities
    
    Signed-off-by: Darshan Gada <gadadarshan@gmail.com>

--- sql/countries.sql
+++ sql/countries.sql
@@ -75,4 +75,4 @@ INSERT INTO `countries` VALUES (1,'Afghanistan','AFG','004','AF','93','Kabul','A
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-05  8:09:56
+-- Dump completed on 2025-08-06 10:01:54
