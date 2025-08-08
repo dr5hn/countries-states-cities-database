045a8c68dd942eb83fa42d1c581e2ca215c06061
dr5hn <6929121+dr5hn@users.noreply.github.com>
2025-08-08 13:26:09 +0000

    📦 Export database formats - 08th Aug 2025
    ✅ All export formats completed successfully:
    - JSON/XML/YAML/CSV: Structured data exports
    - MySQL/PostgreSQL: SQL dump exports
    - SQLite/DuckDB: Database file exports
    - SQL Server/MongoDB: Alternative format exports
    
    📊 Total records: 250 countries, 5128 states, 151233 cities
    
    Signed-off-by: Darshan Gada <gadadarshan@gmail.com>

--- sql/subregions.sql
+++ sql/subregions.sql
@@ -54,4 +54,4 @@ INSERT INTO `subregions` VALUES (1,'Northern Africa','{\"br\": \"Afrika an North
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-06 10:01:54
+-- Dump completed on 2025-08-08 13:10:54
