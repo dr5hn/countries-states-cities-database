135ba7d0b00dfef04df2c2be25297f9b03737b2f
dr5hn <6929121+dr5hn@users.noreply.github.com>
2026-06-21 10:43:39 +0000

    Export database formats - June 21, 2026
    Total records: 250 countries, 5308 states, 152967 cities, 844248 postcodes
    Large files (.gz) uploaded to GitHub Releases instead of git.
    
    Signed-off-by: Darshan Gada <gadadarshan@gmail.com>

--- bin/db/schema.sql
+++ bin/db/schema.sql
@@ -20,11 +20,11 @@ DROP TABLE IF EXISTS `countries`;
 DROP TABLE IF EXISTS `subregions`;
 DROP TABLE IF EXISTS `regions`;
 
--- MySQL dump 10.13  Distrib 8.0.45, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.46, for Linux (x86_64)
 --
 -- Host: localhost    Database: world
 -- ------------------------------------------------------
--- Server version	8.0.45-0ubuntu0.24.04.1
+-- Server version	8.0.46-0ubuntu0.24.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -233,6 +233,6 @@ CREATE TABLE `postcodes` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-05-27  8:46:22
+-- Dump completed on 2026-06-21 10:32:27
 
 SET FOREIGN_KEY_CHECKS=1;
